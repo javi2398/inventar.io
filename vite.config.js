4f30477c1df1d6ede485f090ccf33d40f6e0ecd7
Javi2398 <javi2398@gmail.com>
2025-09-14 20:16:00 +0200

    cambio en vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export default defineConfig({
     plugins: [
@@ -16,17 +13,19 @@ export default defineConfig({
     build: {
         outDir: "public/build",
         emptyOutDir: true,
-        manifest: true,
+        manifest: "manifest.json",
         rollupOptions: {
-            input: "resources/js/app.jsx",
+            input: {
+                app: "resources/js/app.jsx",
+            },
         },
     },
     server: {
-        host: process.env.VITE_HOST || "localhost",
-        port: process.env.VITE_PORT || 5173,
+        host: "localhost",
+        port: 5173,
         strictPort: false,
         cors: {
-            origin: process.env.VITE_CORS_ORIGIN || "http://inventar.io.local",
+            origin: "http://localhost",
             credentials: true,
         },
     },
